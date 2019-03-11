aa54b2717cb93f0fbff9aef68e87c5605fe4bb0f
Rollmasterkush <41843717+Rollmasterkush@users.noreply.github.com>
2019-03-11 11:40:51 -0600

    Add files via upload

--- assets/js/function.js
+++ assets/js/function.js
@@ -43,18 +43,18 @@ jQuery(document).ready(function($){
 
 if (sessionStorage.getItem('advertOnce') !== 'true') {
 //sessionStorage.setItem('advertOnce','true');
-  $('.box','.overlay-verify').show();
+  $('.box, .overlay-verify').show();
 }else{
-  $('.box','.overlay-verify').hide();
+  $('.box, .overlay-verify').hide();
 }
  
 $('#refresh-page').on('click',function(){
-$('.box','.overlay-verify').hide();
+$('.box, .overlay-verify').hide();
 sessionStorage.setItem('advertOnce','true');
 });
   
 $('#reset-session').on('click',function(){
-$('.box','.overlay-verify').show();
+$('.box, .overlay-verify').show();
 sessionStorage.setItem('advertOnce','');
 });
  
